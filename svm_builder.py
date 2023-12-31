576f35b9edc5da64dbff799f228021d21ec4c379
cavalry-cl <136317477+cavalry-cl@users.noreply.github.com>
2023-12-31 11:22:05 +0800

    Update svm_builder.py

--- svm_builder.py
+++ svm_builder.py
@@ -3,9 +3,9 @@ from sklearn import svm
 
 def train_svm_model(X, y):
     train_data,test_data,train_label,test_label=train_test_split(X,y,random_state=1,train_size=0.7,test_size=0.3)
-    model = svm.SVC(kernel="linear", decision_function_shape="ovr")
+    model = svm.LinearSVC(dual='auto', max_iter= 10000, tol=1e-3)
     model.fit(train_data, train_label.ravel())
     train_score = model.score(train_data,train_label)
-    print("训练集：",train_score)
+    print("training set score:",train_score)
     test_score = model.score(test_data,test_label)
-    print("测试集：",test_score)
+    print("testing set score:",test_score)

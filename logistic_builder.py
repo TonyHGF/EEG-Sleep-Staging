f37d76856034b240bfc10282db4449192adc1859
TonyHGF <hugf2022@shanghaitech.edu.cn>
2024-01-03 11:01:53 +0800

    Cross-Validation finished

--- logistic_builder.py
+++ logistic_builder.py
@@ -1,6 +1,8 @@
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
-# just for testing
+from sklearn.model_selection import cross_val_score
+import numpy as np
+
 def train_logistic_model(X, y):
     train_data,test_data,train_label,test_label=train_test_split(X,y,random_state=1,train_size=0.7,test_size=0.3)
     model = LogisticRegression(max_iter=1000, multi_class='ovr', solver='newton-cg')
@@ -10,3 +12,12 @@ def train_logistic_model(X, y):
     test_score = model.score(test_data,test_label)
     print("Testing set score of logistic:",test_score)
     return model
+
+def train_logistic_model_with_cross_validation(X, y):
+    model = LogisticRegression(max_iter=1000, multi_class='ovr', solver='newton-cg')
+
+    # 10-fold cross-validation
+    scores = cross_val_score(model, X, y, cv=10)
+
+    print("Cross-Validation scores:", scores)
+    print("Mean score:", np.mean(scores))
--- pre/draft.md
+++ pre/draft.md
@@ -5,4 +5,20 @@
 - Sleep stage analysis is crucial for understanding sleep patterns and overall health. 
 
 - Sleep stage can be classified by brain waves.
-- Electroencephalogram (EEG) is a medical test used to measure and record the electrical activity in the brain.
+
+- Electroencephalogram (EEG) is a medical test used to measure and record the electrical activity in the brain, i.e. brain waves.
+
+  [^1]: https://en.wikipedia.org/wiki/Electroencephalography
+
+  
+
+<img src="C:\Users\Tony\AppData\Roaming\Typora\typora-user-images\image-20240102211824637.png" alt="image-20240102211824637" style="zoom:50%;" />
+
+
+
+
+
+引用文献一览：
+
+- Hjorth参数特征：Bo Hjorth. EEG analysis based on time domain properties[J]. Electroencephalography and Clinical Neurophysiology, 1970, 29: 306-310.
+- 
